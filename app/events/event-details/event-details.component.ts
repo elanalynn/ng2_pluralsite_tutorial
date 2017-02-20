06ef08643b6837cea9381d36d5c70622618f18da
Elana Kopelevich <ekopelevich@gmail.com>
2017-02-20 13:32:55 -0700

    Create and add event model
    Create event.model with IEvent and ISession models. Specify type in several components for the sake of type safety. Adjust data in EVENTS array to use Dates instead of strings.

--- app/events/event-details/event-details.component.ts
+++ app/events/event-details/event-details.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core'
 import { EventsService } from '../shared/events.service'
 import { ActivatedRoute } from '@angular/router'
+import { IEvent } from '../shared/index'
 
 @Component({
   templateUrl: 'app/events/event-details/event-details.component.html',
@@ -8,7 +9,7 @@ import { ActivatedRoute } from '@angular/router'
 })
 
 export class EventDetailsComponent {
-  event:any
+  event:IEvent
 
   constructor(private eventsService:EventsService, private route:ActivatedRoute){
 
--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core'
 import { EventsService } from './shared/events.service'
 import { ToastrService } from '../common/toastr.service'
 import { ActivatedRoute } from '@angular/router'
+import { IEvent } from './shared/index'
 
 @Component({
   templateUrl: 'app/events/events-list.component.html'
 })
 
 export class EventsListComponent implements OnInit {
-  events:any
+  events:IEvent[]
   constructor(private eventsService:EventsService, private toastr:ToastrService, private route:ActivatedRoute){
 
   }
