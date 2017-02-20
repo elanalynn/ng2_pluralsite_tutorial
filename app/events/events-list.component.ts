d84ef07de57a543b4600275cddfab897f225295d
Elana Kopelevich <ekopelevich@gmail.com>
2017-02-20 10:33:40 -0700

    change timeout back to 100

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { EventsService } from './shared/events.service'
 import { ToastrService } from '../common/toastr.service'
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   templateUrl: 'app/events/events-list.component.html'
@@ -8,11 +9,11 @@ import { ToastrService } from '../common/toastr.service'
 
 export class EventsListComponent implements OnInit {
   events:any
-  constructor(private eventsService:EventsService, private toastr:ToastrService){
+  constructor(private eventsService:EventsService, private toastr:ToastrService, private route:ActivatedRoute){
 
   }
   ngOnInit(){
-    this.eventsService.getEvents().subscribe(events => this.events = events)
+    this.events = this.route.snapshot.data['events']
   }
   handleThumbnailClick(eventName){
     this.toastr.success(eventName)
